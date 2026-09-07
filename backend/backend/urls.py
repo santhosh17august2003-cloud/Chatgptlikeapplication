8685aa59ccb10414a03fbc8751a912dae8b01c6f
Santhosh L <santhosh17august2003@gmail.com>
2026-09-07 06:54:08 +0530

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
 
 def home(request):
     return JsonResponse({
